ab69e20b731f8d9c64aa69217ebd28531a25ea0d
Marina Savelyeva <mar.saveleva.18@gmail.com>
2024-04-21 17:39:25 +0300

    problem

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,86 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
 import '../../index.css';
 import styles from './app.module.css';
+import {
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+  BrowserRouter
+} from 'react-router-dom';
 
-import { AppHeader } from '@components';
+const AppRoute = () => {
+  //для модалки
+  const location = useLocation();
+  const backgroundLocation = location.state?.backgroundLocation;
+
+  //для навигации назад, на -1
+  const navigate = useNavigate();
+  const Back = () => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <Routes location={backgroundLocation || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title={'1'} onClose={Back}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title={'2'} onClose={Back}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title={'3'} onClose={Back}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </>
+  );
+};
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <AppRoute />
   </div>
 );
 
